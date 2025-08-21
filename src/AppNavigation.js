57ec52fff6c7bc6581191bf8853a7a2d8a0eb798
naveen kumar <nk1436350@gmail.com>
2025-08-21 06:45:45 +0530

    splash screen complete and create a new component Home.js

--- src/AppNavigation.js
+++ src/AppNavigation.js
@@ -3,12 +3,15 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Splash from './screens/Splash';
+import Home from './screens/Home';
+
 const stack = createNativeStackNavigator();
 const AppNavigation = () => {
   return (
     <NavigationContainer>
       <stack.Navigator>
-        <stack.Screen name="Splash" component={Splash} />
+        <stack.Screen name="Splash" component={Splash} options={{headerShown:false}}/>
+        <stack.Screen name="Home" component={Home} options={{headerShown:false}}/>
       </stack.Navigator>
     </NavigationContainer>
   );
--- src/screens/Splash.js
+++ src/screens/Splash.js
@@ -1,11 +1,16 @@
 import { StyleSheet, Text, View,Image } from 'react-native';
-import React from 'react';
+import React, { useEffect } from 'react';
 
-const Splash = () => {
+const Splash = ({navigation}) => {
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.navigate('Home')
+    },3000)
+  },[])
   return (
     <View style={styles.container}>
-      <Image source={require('../Images/logo.png')} style={styles.logo } />
-      <Text>Docs here</Text>
+      <Image source={require('../Images/logo1.png')} style={styles.logo } />
+      <Text style={styles.logoTitle}>Doctors here</Text>
 
     </View>
   );
@@ -18,9 +23,17 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor:'#252628ff'
   },
   logo: {
     width: 100,
-    height:100
+    height: 100,
+    tintColor:'#fff'
+  },
+  logoTitle: {
+    fontWeight: '700',
+    color: '#fff',
+    fontSize: 22,
+    
   }
 });
